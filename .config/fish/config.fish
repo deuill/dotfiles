4d16129638185492e5a12284cadbc639a9de05de
Alex Palaistras <alex@deuill.org>
2016-02-21 14:51:31 +0000

    Add configuration files for `fish` shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+# Set global variables
+set -x PATH $PATH $HOME/.go/bin
+set -x GOPATH $HOME/.go
+
+# Disable greeting text
+set fish_greeting
+
+# Custom theme.
+set theme 'shellder'
+source $HOME/.config/fish/themes/$theme.fish
+
+# Custom aliases
+alias sc 'ag --pager "less -R"'
+alias emacs 'emacs -mm'
+alias trash 'gvfs-trash'
+alias dot 'git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+function ciff
+	diff -u $argv | colordiff | less -R
+end
