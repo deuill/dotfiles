1b77597ad6177b40de938c732b526f91caf33493
Alex Palaistras <apalaistras@cloudflare.com>
2021-12-21 14:21:45 +0000

    Remove mail-related files
    The Notmuch/Mu-based mail pipeline was largely experimental, and was
    never really completed or used. Removing this until we revisit with
    better tooling.

--- .config/astroid/poll.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Synchronize all configured accounts.
-
-for account in $(basename ${HOME}/.config/mail/account.d/*)
-do
-	${HOME}/.local/bin/mail-sync fetch ${account%.conf} || exit $?
-done
